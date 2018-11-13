2161fd2667bf48a888ddd0896705f4089964a7ed
Will <wiiliamchung360@gmail.com>
2018-11-13 12:56:45 -0800

    update readme with setup instructions + startup script

---
+++ chrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+chromium --disable-infobars --kiosk 'localhost'
+
+xdotool key ctrl+alt+BackSpace
